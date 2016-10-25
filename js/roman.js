0ac3c26066de950f8f6677c7ec641be09b30e2f6
undefined8 <dzamilatskiy2019@bklawtech.com>
2016-10-25 22:03:25 +0000

    roman numeral project

--- js/roman.js
+++ js/roman.js
@@ -17,8 +17,49 @@ $(function() {
   // 1   | I
 
   function convertToRomanNumeral(number) {
+    if(isNaN(number))
+      return "Not a number!";
+      
     console.log("Number = " + number);
     var output = "";
+    var input = parseInt(number);
+  
+    if(input <= 0)
+      return "less than 1";
+    else if(input > 100)
+      return "greater than 100";
+  
+    while(input > 0){
+      if(input >= 100){
+        output += "C";
+        input -= 100;
+      }else if(input >= 90){
+        output += "XC";
+        input -= 90;
+      }else if(input >= 50){
+        output += "L";
+        input -= 50;
+      }else if(input >= 40){
+        output += "XL";
+        input -= 40;
+      }else if(input >= 10){
+        output += "X";
+        input -= 10;
+      }else if(input >= 9){
+        output += "IX";
+        input -= 9;
+      }else if(input >= 5){
+        output += "V";
+        input -= 5;
+      }else if(input >= 4){
+        output += "IV";
+        input -= 4;
+      }else if(input >= 1){
+        output += "I";
+        input -= 1;
+      }
+    }
+
     return output;
   }
 
@@ -31,7 +72,7 @@ $(function() {
   var $numberInput = $("#number_input");
   var $romanNumeralOutput = $("#roman_numeral_output");
 
-  $button.on("click", function(e){
+  $button.on("click", function(e) {
     e.preventDefault();
     numberVal = $numberInput.val();
     romanNumeralVal = convertToRomanNumeral(numberVal);
